544737ceaeea1391caaef082a4df57f4e2e5c295
DilanNuma <dilanmatheonuma@gmail.com>
2024-04-22 03:00:55 -0500

    solved challenge 1

--- Reto1.js
+++ Reto1.js
@@ -3,44 +3,39 @@
 // [usuario]: correo
 
 let BaseDeUsuarios = []
+let nombre = prompt("Ingrese su Nombre: ").toLowerCase();
+let apellido = prompt("Ingrese su Apellido: ").toLowerCase();
+let nombreCompleto = nombre.concat(' ', apellido)
+let nom = nombre.slice(0, 3)
+let ape = apellido.slice(0, 3)
+let usuario = nom.concat(ape)
+let correo = usuario.concat('@myDomain.com')
 
-function agregarUsuario() {
-    let nombre = prompt("Ingrese su Nombre: ").toLowerCase();
-    let apellido = prompt("Ingrese su Apellido: ").toLowerCase();
-    let nombreCompleto = nombre.concat(' ', apellido)
-    let nom = nombre.slice(0, 3)
-    let ape = apellido.slice(0, 3)
-    let usuario = nom.concat(ape)
-    let correo = usuario.concat('@myDomain.com')
+console.log(nombreCompleto);
 
-    console.log(nombreCompleto);
-
-    let contador = 1
-    Object.keys(BaseDeUsuarios).forEach(usuarioExiste => {
-        if (usuarioExiste === usuario) {
-            usuario = usuario + contador
-            contador++
-        }
-    });
-
-    BaseDeUsuarios[usuario] = correo
-
-    console.log(`Nombre de Usuario (username): ${usuario}`)
-    console.log(`Correo Electrónico: ${correo}`)
-
-    let continuar = prompt("¿Deseas agregar otro usuario? (si/no)").toLowerCase();
-    if (continuar === "si") {
-        agregarUsuario()
-    } else {
-        console.log("Base de Usuarios:")
-        console.table(BaseDeUsuarios)
+let contador = 1
+Object.keys(BaseDeUsuarios).forEach(usuarioExiste => {
+    if (usuarioExiste === usuario) {
+        usuario = usuario + contador
+        contador++
     }
+});
 
-    const keys = Object.keys(BaseDeUsuarios);
-    console.log(keys);
+BaseDeUsuarios[usuario] = correo
 
-    let isElementIncluded = keys.includes(usuario);
-    console.log(isElementIncluded);
+console.log(`Nombre de Usuario (username): ${usuario}`)
+console.log(`Correo Electrónico: ${correo}`)
+
+let continuar = prompt("¿Deseas agregar otro usuario? (si/no)").toLowerCase();
+if (continuar === "si") {
+    agregarUsuario()
+} else {
+    console.log("Base de Usuarios:")
+    console.table(BaseDeUsuarios)
 }
 
-agregarUsuario();
+const keys = Object.keys(BaseDeUsuarios);
+console.log(keys);
+
+let isElementIncluded = keys.includes(usuario);
+console.log(isElementIncluded);
